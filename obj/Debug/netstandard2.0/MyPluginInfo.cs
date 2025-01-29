218b2cdbff65b0dece2ff1b73147a83250cb5ab3
EliottDup <45095693+EliottDup@users.noreply.github.com>
2025-01-29 16:49:53 +0100

    Delete obj directory

--- obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PeaksOfArchipelago
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "nl.eliott.PeaksOfArchipelago";
-        public const string PLUGIN_NAME = "Peaks Of Archipelago";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
