03356a2b50a680b2a404028f6aa08f8e124ce50b
EliottDup <45095693+EliottDup@users.noreply.github.com>
2025-02-01 11:11:41 +0100

    redid all the formatting to be a bit clearer

--- PeaksOfArchipelago.cs
+++ PeaksOfArchipelago.cs
@@ -18,7 +18,7 @@ namespace PeaksOfArchipelago;
 public class PeaksOfArchipelago : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
+
     private void Awake()
     {
         // Plugin startup logic
@@ -31,41 +31,48 @@ public class PeaksOfArchipelago : BaseUnityPlugin
     }
 }
 
-public class PeaksOfArchipelagoMod : ModClass {
-    [Editable] public string Hostname {get; set;} = "archipelago.gg";
-    [Editable] public string Port {get; set;} = "";
-    [Editable] public string SlotName {get; set;} = "";
-    [Editable] public string Password {get; set;} = "";
-    [Editable] public bool AutoConnect {get; set;} = false;
-    [Editable] public UnityEvent Connect {get; set;} = new UnityEvent();
+public class PeaksOfArchipelagoMod : ModClass
+{
+    [Editable] public string Hostname { get; set; } = "archipelago.gg";
+    [Editable] public string Port { get; set; } = "";
+    [Editable] public string SlotName { get; set; } = "";
+    [Editable] public string Password { get; set; } = "";
+    [Editable] public bool AutoConnect { get; set; } = false;
+    [Editable] public UnityEvent Connect { get; set; } = new UnityEvent();
 
-    Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID); 
+    Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
     public ArchipelagoSession session = null;
 
-    public override void OnEnabled(){
+    public override void OnEnabled()
+    {
         harmony.PatchAll();
         Connect.AddListener(OnConnect);
-        if(AutoConnect){
+        if (AutoConnect)
+        {
             ConnectFunc();
         }
         Debug.Log("Loaded Peaks of Archipelago!");
     }
 
-    public override void Start() {
+    public override void Start()
+    {
         // DO NOT PUT GAMEMANAGER.SAVE HERE IT FUCKED UP MY SAVES LOL
         Debug.Log("Now in scene: " + SceneManager.GetActiveScene().name);
     }
 
-    public override void OnDisabled() {
+    public override void OnDisabled()
+    {
         harmony.UnpatchSelf();
     }
 
-    private void OnConnect() {
+    private void OnConnect()
+    {
         ConnectFunc();
     }
 
-    private bool ConnectFunc(){
+    private bool ConnectFunc()
+    {
         Debug.Log("Connecting to " + GetHostNamePort());
         session = ArchipelagoSessionFactory.CreateSession(GetHostNamePort());
         session.SetClientState(Archipelago.MultiClient.Net.Enums.ArchipelagoClientState.ClientReady);
@@ -73,60 +80,76 @@ public class PeaksOfArchipelagoMod : ModClass {
         return false;
     }
 
-    private string GetHostNamePort() {
+    private string GetHostNamePort()
+    {
         return Hostname + ":" + Port;
     }
 
     // Harmony Patches
     [HarmonyPatch(typeof(StamperPeakSummit), "StampJournal")]
-    public class PeakSummitedPatch{
-        static void Prefix(StamperPeakSummit __instance){
+    public class PeakSummitedPatch
+    {
+        static void Prefix(StamperPeakSummit __instance)
+        {
             Debug.Log("stamping: " + __instance.peakNames.ToString());
-            
+
         }
 
-        static void Postfix(StamperPeakSummit __instance) {
+        static void Postfix(StamperPeakSummit __instance)
+        {
             Debug.Log("done stamping: " + __instance.peakNames.ToString());
         }
     }
 
     [HarmonyPatch(typeof(FallingEvent), "FellToDeath")]
-    public class FellToDeathPatch(){
-        static void Postfix(){
+    public class FellToDeathPatch
+    {
+        static void Postfix()
+        {
             Debug.Log("Died");
         }
     }
 
     [HarmonyPatch(typeof(EnterPeakScene), "Awake")]
-    public class EnterPeakScenePatch{
-        static void Postfix(EnterPeakScene __instance){
+    public class EnterPeakScenePatch
+    {
+        static void Postfix(EnterPeakScene __instance)
+        {
             Debug.Log("Entering: " + GameObject.FindGameObjectWithTag("SummitBox").GetComponent<StamperPeakSummit>().peakNames.ToString());
         }
     }
 
     [HarmonyPatch(typeof(ArtefactOnPeak), "PickUpItem")]
-    public class ArtefactOnPeakPatch{
-        static void Postfix(ArtefactOnPeak __instance){
+    public class ArtefactOnPeakPatch
+    {
+        static void Postfix(ArtefactOnPeak __instance)
+        {
             Debug.Log("Picked Up: " + __instance.peakArtefact);
             ArtefactOnPeak.Artefacts artefact = __instance.peakArtefact;
         }
     }
 
     [HarmonyPatch(typeof(RopeCollectable), "PickUpRope")]
-    public class RopeCollectablePatch(){
-        static void Postfix(RopeCollectable __instance){
+    public class RopeCollectablePatch
+    {
+        static void Postfix(RopeCollectable __instance)
+        {
             Ropes rope = Utils.GetRopeCollectable(__instance);    //CHECK FOR ROPE
             Debug.Log("Picked Up rope: " + rope);
         }
     }
 
     [HarmonyPatch(typeof(RopeAnchor), "Start")]
-    public class StartTranspiler(){
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions){
+    public class StartTranspiler
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
             Debug.Log("Transpiling RopeAnchor.Start");
             var codes = new List<CodeInstruction>(instructions);
-            for (int i = 0; i < codes.Count; i++){
-                if (i == 76 || i == 77  || i == 78){
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (i == 76 || i == 77 || i == 78)
+                {
                     Debug.Log("Disabled line " + i + " : " + codes[i].ToString());
                     continue;
                 }
@@ -136,12 +159,16 @@ public class PeaksOfArchipelagoMod : ModClass {
     }
 
     [HarmonyPatch(typeof(RopeAnchor), "DetachThenAttachToNew", MethodType.Enumerator)]
-    public class DetachThenAttachToNewTranspiler(){
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions){
+    public class DetachThenAttachToNewTranspiler
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
             Debug.Log("Transpiling DetachThenAttachToNew");
             var codes = new List<CodeInstruction>(instructions);
-            for (int i = 0; i < codes.Count; i++){
-                if (i == 120 || i == 121 || i == 122 || i == 123){
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (i == 120 || i == 121 || i == 122 || i == 123)
+                {
                     Debug.Log("Disabled line " + i + " : " + codes[i].ToString());
                     continue;
                 }
@@ -150,13 +177,17 @@ public class PeaksOfArchipelagoMod : ModClass {
         }
     }
 
-        [HarmonyPatch(typeof(RopeAnchor), "PullOutAnchor", MethodType.Enumerator)]
-    public class PullOutAnchorTranspiler(){
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions){
+    [HarmonyPatch(typeof(RopeAnchor), "PullOutAnchor", MethodType.Enumerator)]
+    public class PullOutAnchorTranspiler
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
             Debug.Log("Transpiling PullOutAnchor");
             var codes = new List<CodeInstruction>(instructions);
-            for (int i = 0; i < codes.Count; i++){
-                if (i == 79 || i == 80 || i == 81 || i == 82){
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (i == 79 || i == 80 || i == 81 || i == 82)
+                {
                     Debug.Log("Disabled line " + i + " : " + codes[i].ToString());
                     continue;
                 }
@@ -166,12 +197,16 @@ public class PeaksOfArchipelagoMod : ModClass {
     }
 
     [HarmonyPatch(typeof(RopeCollectable), "CheckRope")]
-    public class CheckRopeTranspiler(){
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions){
+    public class CheckRopeTranspiler
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
             Debug.Log("Transpiling CheckRope");
             var codes = new List<CodeInstruction>(instructions);
-            for (int i = 0; i < codes.Count; i++){
-                if (i == 267 || i == 268 || i == 269 || i == 270){
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (i == 267 || i == 268 || i == 269 || i == 270)
+                {
                     Debug.Log("Disabled line " + i + " : " + codes[i].ToString());
                     continue;
                 }
